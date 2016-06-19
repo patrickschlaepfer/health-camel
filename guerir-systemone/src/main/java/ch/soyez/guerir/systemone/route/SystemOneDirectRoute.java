f223581d32a469bf15b54c5751d047ad0e14cccd
Patrick Schlaepfer <patrick@schlaepfer.com>
2016-06-19 16:31:36 +0200

    Translator HL7Pid to Person added.

--- guerir-systemone/src/main/java/ch/soyez/guerir/systemone/route/SystemOneDirectRoute.java
+++ guerir-systemone/src/main/java/ch/soyez/guerir/systemone/route/SystemOneDirectRoute.java
@@ -3,6 +3,7 @@ package ch.soyez.guerir.systemone.route;
 import org.apache.camel.builder.RouteBuilder;
 
 import ch.soyez.guerir.systemone.translator.SystemOneHL7In;
+import ch.soyez.guerir.translator.HL7PidToPatient;
 
 public class SystemOneDirectRoute extends RouteBuilder {
 
@@ -16,6 +17,7 @@ public class SystemOneDirectRoute extends RouteBuilder {
   			.unmarshal()
   			.hl7()
   			.bean(SystemOneHL7In.class)
+  			.bean(HL7PidToPatient.class)
 			.log("after procssing: ${body}")
   			.end();
 		// @formatter:on
